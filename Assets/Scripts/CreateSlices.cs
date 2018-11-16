c914080a77f1f4da834496162d7be82e35713caa
Jpe230 <pablin.123.ra@gmail.com>
2018-11-16 10:26:02 -0600

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/CreateSlices.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateSlices : MonoBehaviour {
+
+    public GameObject prefab;
+    public Transform parent;
+    public Sprite[] whaleParts;
+    public List<GameObject> slices;
+
+    public int NumberOfSlices;
+
+    private bool isTransparent = false;
+  
+	void Start () {
+
+        GameObject oldSlice = null;
+
+        for(int x = 0; x < NumberOfSlices; x++)
+        {
+            var slice = Instantiate(prefab, parent, false);
+
+            slice.GetComponent<Image>().sprite = whaleParts[(whaleParts.Length - 1) - x ];
+            slice.GetComponent<FollowCursor>().moveSpeed = 5f;
+            slice.GetComponent<FollowCursor>().isFirst = false;
+
+            if (x != 0)
+            {
+                oldSlice.GetComponent<FollowCursor>().nextPos = slice.GetComponent<Transform>();
+            }
+
+            if (x == NumberOfSlices - 1)
+            {
+                slice.GetComponent<FollowCursor>().isFirst = true;
+                slice.GetComponent<FollowCursor>().moveSpeed = .02f;
+            }
+            slices.Add(slice);
+            oldSlice = slice;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (!isTransparent)
+            {
+                for (int x = 0; x < slices.Count; x++)
+                {
+                    slices[x].GetComponent<Image>().color = new Color(1, 1, 1, .1f);
+                }
+            }
+            else
+            {
+                for (int x = 0; x < slices.Count; x++)
+                {
+                    slices[x].GetComponent<Image>().color = new Color(1, 1, 1, 1f);
+                }
+            }
+
+            isTransparent = !isTransparent;
+        }
+       
+    }
+}
---
+++ Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour {
+
+    RectTransform rectTranform;
+    private Vector3 mousePosition;
+    public Transform nextPos;
+    public float moveSpeed = .1f;
+
+
+    public bool isFirst = false;
+
+
+	void Update () {
+        if (isFirst)
+        {
+            mousePosition = Input.mousePosition;
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+        else
+        {
+            transform.position = Vector2.Lerp(transform.position, nextPos.position, moveSpeed);
+        }
+       
+    }
+}
